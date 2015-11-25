5902a0440a9c3393230b1126f4fe346a8f97be84
Ichiwa <thankyou46585960@gmail.com>
2015-11-25 10:41:17 +0900

    added APPID to Query parameters

--- app/src/main/java/example/com/rxandroidexample/WeatherApi.java
+++ app/src/main/java/example/com/rxandroidexample/WeatherApi.java
@@ -8,6 +8,6 @@ import rx.Observable;
 
 public interface WeatherApi {
     // http://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp
-    @GET("/data/2.5/weather")
+    @GET("/data/2.5/weather?APPID=d0ca227fdc9e831a32c172faae08b6d6")
     public Observable<JSONObject> get(@Query("q") String query);
 }
